a6f43edbf9e1fd76626ea66a6794e22799014200
0xInu <calin.cretiu@gmail.com>
2022-07-21 19:54:18 +0300

    file write/read test

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
+const fs = require("fs");
+
 router.get("/", (req, res) => {
-  res.send("asd");
+  let transactions = [{ name: "john Snow" }];
+  let jsonTransactions = JSON.stringify(transactions);
+  fs.writeFile(`myFile.json`, jsonTransactions, "utf8", function (err) {
+    if (err) {
+      console.log("error");
+      return console.log(err);
+    }
+    console.log("JSON Saved");
+    const json = require("../myFile.json");
+    res.send(json);
+  });
 });
 module.exports = router;
